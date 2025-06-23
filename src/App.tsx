dffaedb1386d194dac2d978725afd2407c1fe99d
Dharmesh Yadav <dharmeshyadav3434@gmail.com>
2025-06-23 22:50:04 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-400 flex justify-around">Hello</h1>
+      <div className="flex justify-around">
+        <button className="btn btn-primary">TEXT</button>
+      </div>
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
+    </>
+  );
+}
+
+export default App;
